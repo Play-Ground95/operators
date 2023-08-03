c2cbd59b29c2a094eb2beb25caef5c724da93acf
Kavindu95 <kavindueng95@gmail.com>
2023-08-03 20:06:49 +0530

    test3

--- Test.java
+++ Test.java
@@ -1,6 +1,30 @@
 public class Test {
     public static void main(String[] args) {
-        int a =2;
-        int b =5;
+        // int a =2;
+        // int b =3;
+        // int result = a++ + b + a++ + --b + ++a;
+        // System.out.println(result);
+        ///////////////////////////////////////////////
+        // int x=1;
+        // int y=2;
+
+        // int result1= ++x + ++y + x - y + x++ - --y;
+        // result=result1+ x++ + y--;
+        // result=x + result + y;
+        // System.out.printf("result=%S ,x=%s, y=%s \n",result,x,y);
+        ////////////////////////////////////////////////////////////////////////////
+        
+        int x=5;
+        int y=x;
+        
+        y=y++ + ++x + x++ + -y +x-- +  -x--;
+         x=x++ + ++y +2 + y-- + -x-- + x++;
+        
+        int result =x++ - --y;
+        
+        System.out.printf("result=%S ,x=%s, y=%s \n",result,x,y);
+
+
+        
     }
 }
