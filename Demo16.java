971bd715c644b7578cf3ed5102aed873c52f8c3b
Kavindu95 <kavindueng95@gmail.com>
2023-08-05 14:03:44 +0530

    demo16

--- Demo16.java
+++ Demo16.java
@@ -1,13 +1,15 @@
 public class Demo16 {
     public static void main(String[] args) {
-        byte x =2;
-        int y=x <<1;
-        y=y+x;
+        byte x = 2; //0010
+        int y = x << 1; //0100 =4
+        y = y + x; //4+2 =6
         System.out.println(y);
 
-        byte a=5;
-        int b= y>>>2;
-        b=a+b;
+        System.out.println();
+
+        byte a = 5;             // 0101
+        int b = a >>> 2;        // 01
+        b = a + b;
         System.out.println(b);
 
     }
