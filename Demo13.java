70f37160131f1069f4f5c44c29df7d467aa64df7
Kavindu95 <kavindueng95@gmail.com>
2023-08-05 12:40:33 +0530

    demo13

--- Demo13.java
+++ Demo13.java
@@ -1,8 +1,14 @@
 public class Demo13 {
     public static void main(String[] args) {
-        byte myByte=-127;
-        byte myByte2= (byte)(myByte<<1);
+        byte myByte=-20;
+        byte myByte2= (byte)(myByte<<1); //first of all we get the binary value of -20(from2s complement) --> after we can shift in one digit -->and convert to decimal
+
+        //EX- -20=11101100 -->after shifted= 11011000 =-40 
+
+
+
         short myShort=(short)(myByte<<1);
+        
         System.out.println(myByte2);
         System.out.println(myShort);
 
