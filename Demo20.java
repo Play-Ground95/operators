6d975dd99da5bc7576bac26a268df9e8d3303f4b
Kavindu95 <kavindueng95@gmail.com>
2023-08-05 14:18:29 +0530

    demo20

--- Demo20.java
+++ Demo20.java
@@ -1,17 +1,26 @@
 public class Demo20 {
     public static void main(String[] args) {
-        int x=5;
-        int y=2;
+        
+        int x = 5;
+        int y = 2;
 
-        boolean result=x>y;
+        boolean result = x > y;
         System.out.println(result);
 
-        //gt
+        // gt   >
+        // lt   <
+        // gte  >= //greter than or equal
+        // lte  <= //less than or equal
+        System.out.println();
 
-        class Animal {}
-        class Dog extends Animal{}
-        class Box{}
-
+        System.out.println(new Dog() instanceof Dog);
         System.out.println(new Dog() instanceof Animal);
-    }m,.
+        // System.out.println(new Box() instanceof Animal);
+        System.out.println(new Box() instanceof Object);
+    }
 }
+
+class Animal {} //that is parent class
+class Dog extends Animal {} //that was inherited by parent class. //hence that is true. dog is a animal
+class Cat extends Animal {}
+class Box {}
