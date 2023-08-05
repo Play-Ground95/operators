b39b593ffea08e2200f17bb435cc0535e8217123
Kavindu95 <kavindueng95@gmail.com>
2023-08-05 12:48:33 +0530

    demo14

--- Demo14.java
+++ Demo14.java
@@ -1,7 +1,7 @@
 public class Demo14 {
     public static void main(String[] args) {
-        byte myByte=10;
-        byte myByte2= (byte)(myByte>>2);
+        byte myByte=10; //for postive values we cannot need the 2s complement. need a bonary value of 10 and its filled in int after we can shifted from 2
+        byte myByte2= (byte)(myByte<<2);
         short myShort=(short)(myByte>>2);
         
         byte myByte3 =-127;
