587984f4f4af08f2fd0657496c40a75bf928f6aa
Kavindu95 <kavindueng95@gmail.com>
2023-08-05 14:05:20 +0530

    demo17&18

--- Demo18.java
+++ Demo18.java
@@ -1,13 +1,17 @@
 public class Demo18 {
     public static void main(String[] args) {
-        byte a =-128;
-        int b =~a>>>28;
-        int c=b*2 + ++a;
-        System.out.println(c);
+        byte a = -128;
+        int b = ~a >>> 28;
+        // System.out.println(b);
+        int c = b * 2 + ++a;
+        System.out.println(c); 
 
-        byte myByte= ~0;
-        int myInt = myByte>> (31-myByte);
-        myInt =myInt<<1;
+        System.out.println();
+
+        byte myByte = ~0;
+        int myInt = myByte >> (31 - myByte);
+        System.out.println(Integer.toBinaryString(myInt));
+        myInt = myInt << 1;
         System.out.println(myInt);
     }
 }
