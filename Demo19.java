961da44869b264db25a3285585dcd07cd9e95c93
Kavindu95 <kavindueng95@gmail.com>
2023-08-05 14:12:33 +0530

    demo19

--- Demo19.java
+++ Demo19.java
@@ -1,14 +1,17 @@
 public class Demo19 {
     public static void main(String[] args) {
-        int x=2;
-        int y= ~x++;
-        char myChar='1';
-        
-        int a= ~~--y +Integer.valueOf(x+myChar+""+~y);
-        System.out.println(a);
+        int x = 2;
+        int y = ~x++;
 
-        // int b= a*x + y + --x <<2 +(myChar);
-        // b=b+++y*3<<1;
-        // System.out.printf("b=%s, a=%s, x=%s, y=%s \n",b,a,x,y);
+        char myChar = '1';
+        
+        int a = ~~--y + Integer.valueOf(x + myChar + "" + ~y);
+
+        int b = a * x + y + --x << 2 + (myChar + "").length();
+        System.out.println(b);
+
+        b = b+++y*3<<1;
+        
+        System.out.printf("b=%s, a=%s, x=%s, y=%s \n", b, a, x, y);
     }
 }
