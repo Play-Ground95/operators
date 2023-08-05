08f1c078ca1f60d73e4cda5ba236fd38bc81d95e
Kavindu95 <kavindueng95@gmail.com>
2023-08-05 10:11:11 +0530

    demo12update

--- Demo12.java
+++ Demo12.java
@@ -1,7 +1,7 @@
 public class Demo12 {
     public static void main(String[] args) {
         byte myByte =10;
-        byte myByte2 =(byte) (myByte<<2);
+        byte myByte2 =(byte) (myByte<<2); //skip the two digit points of the value //but dont effect for the value's
         
         System.out.println(myByte2);
         System.out.printf("%08d \n",Integer.valueOf(Integer.toBinaryString(myByte)));
